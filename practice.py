b7defab15eec3eae4b275879578b92bad7f7b6e1
m.f <86688824+Mahdifp12@users.noreply.github.com>
2021-07-06 22:17:16 +0430

    practice
    this description about my practice

---
+++ practice.py
@@ -0,0 +1,16 @@
+mark_stu = int(input('Enter your is student mark (0 to 20): '))
+
+if mark_stu == 20:
+    print('Really nice')
+
+elif mark_stu >=15:
+    print('Good job')
+
+elif mark_stu >=10:
+    print('your need trying more')
+
+elif mark_stu >=0:
+    print('sorry you rejected ...')
+
+else:
+    print('..!!!..')
